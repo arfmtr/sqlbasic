04fc11493aba25f2468a24e5f841afd9301bdc00
Arifa M <59384158+arfmtr@users.noreply.github.com>
2022-05-18 12:31:36 +0700

    Create database and table

--- createdatabase.sql
+++ createdatabase.sql
@@ -6,7 +6,9 @@ USE basql;
 CREATE TABLE product(
 ID int,
 name_pro varchar(50),
-price decimal(4,2)
+description varchar(100),
+price decimal(4,2),
+cust varchar(20)
 );
 
 --Create a pepole table. People will have a numeric type id, a name, a phone number, a date of birth and a height
