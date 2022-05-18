b880d3f4bb4a6508b7a5f43f4609388868501ef9
Arifa M <59384158+arfmtr@users.noreply.github.com>
2022-05-18 12:46:18 +0700

    Delete data

--- deleting.sql
+++ deleting.sql
@@ -1,1 +1,8 @@
+--Delete the first product
+DELETE FROM product WHERE id = 1;
 
+--Delete all people named "Aphrodite"
+DELETE FROM people WHERE name = 'Aphrodite';
+
+--Wipe all Samsung under 24 ram
+DELETE FROM computer WHERE brand = 'Samsung' AND ram <24;
