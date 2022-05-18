0e8e3789d380fc21ce22c13d3ecea31fea61a926
Arifa M <59384158+arfmtr@users.noreply.github.com>
2022-05-18 12:03:03 +0700

    Update reading.sql

--- reading.sql
+++ reading.sql
@@ -1,1 +1,10 @@
+--Read all product data
+SELECT * FROM product;
 
+--Read just the id and the name of the product
+SELECT id, name FROM product;
+
+--Read phone number in people table
+SELECT pho
+--Read all computer data
+--Read just the brand, ram memory and processor
