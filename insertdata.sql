5470d242bb982eb71d7ed5edaa79f214e8f6659e
Arifa M <59384158+arfmtr@users.noreply.github.com>
2022-05-18 11:55:54 +0700

    Create insertdata.sql

---
+++ insertdata.sql
@@ -0,0 +1,16 @@
+--Add 3 product in the table
+INSERT INTO product VALUES(1, 'Charger','For charge your computer');
+INSERT INTO product VALUES(2, 'Sticker','Beautifully your computer');
+INSERT INTO product VALUES(3, 'Mouse','Drag and drop easier');
+
+--Add 5 students in people table
+INSERT INTO people VALUES(1, 'Benecia','889576',07111987,5.02);
+INSERT INTO people VALUES(2, 'Athena','678927',021218887,4.02);
+INSERT INTO people VALUES(3, 'Zeus','908756',170219989,5.10);
+INSERT INTO people VALUES(4, 'Demeter','818725',110719990,6.02);
+INSERT INTO people VALUES(5, 'Aphrodite','899617',120719997,5.04);
+
+--Add 3 computer in the table, specifying only the brand and processor
+INSERT INTO computer VALUES('Samsung','i5');
+INSERT INTO computer VALUES('Asus','i3');
+INSERT INTO computer VALUES('Compaq','AMDA');
