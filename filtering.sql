58827c7b5b1ca577f0c8838a20958eac9b9c8ac9
Arifa M <59384158+arfmtr@users.noreply.github.com>
2022-05-18 12:36:41 +0700

    Update filtering.sql

--- filtering.sql
+++ filtering.sql
@@ -1,1 +1,11 @@
+--Read all the data of the product bought by "Elle"
+SELECT * FROM product
+WHERE cust = 'Elle';
 
+--Read all data of people who have name "Demeter"
+SELECT * FROM people 
+WHERE name = 'Demeter';
+
+--Read all Asus computer data
+SELECT * FROM computer
+WHERE brand = 'Asus';
