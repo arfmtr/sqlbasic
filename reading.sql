8bbe480cbc80ac9a144adb8113f38e67086afc4c
Arifa M <59384158+arfmtr@users.noreply.github.com>
2022-05-18 12:05:41 +0700

    Reading data

--- reading.sql
+++ reading.sql
@@ -5,6 +5,10 @@ SELECT * FROM product;
 SELECT id, name FROM product;
 
 --Read phone number in people table
-SELECT pho
+SELECT phone FROM people;
+
 --Read all computer data
+SELECT * FROM computer;
+
 --Read just the brand, ram memory and processor
+SELECT brand, ram, processor FROM computer;
